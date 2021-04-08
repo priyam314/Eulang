43b9c2e0304b22e22e7ddc9867aceff119535bab
priyam314 <garg.priyam2015@gmail.com>
2021-04-09 00:36:45 +0530

    tokens.go UPDATED with tokens

--- tokens/tokens.go
+++ tokens/tokens.go
@@ -1,7 +1,63 @@
-package main
+package tokens
 
-import "fmt"
+type TokenType string
 
-func main() {
-	fmt.Println("Hello World")
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	NOT      = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	RT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	VAR      = "VAR"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"function": FUNCTION,
+	"var":      VAR,
+	"True":     TRUE,
+	"False":    FALSE,
+	"if":       IF,
+	"else":     ELSE,
+	"return":   RETURN,
+}
+
+func LookupIndent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
 }
